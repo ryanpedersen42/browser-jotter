877f7a3f4886d42a553bb6839657fe839a63ea92
Ryan Pedersen <ryanpedersen42@gmail.com>
2022-11-10 12:53:07 -0500

    change up linting

---
+++ .eslintrc.cjs
@@ -0,0 +1,37 @@
+module.exports = {
+    extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
+    parser: '@typescript-eslint/parser',
+    plugins: ['@typescript-eslint'],
+    root: true,
+  };
+// module.exports = {
+//     "env": {
+//         "browser": true,
+//         "es2021": true
+//     },
+//     "extends": [
+//         "eslint:recommended",
+//         "plugin:react/recommended",
+//         "plugin:@typescript-eslint/recommended"
+//     ],
+//     "overrides": [
+//     ],
+//     "parser": "@typescript-eslint/parser",
+//     "settings": {
+//         "react": {
+//             "version": "^17.0.2"
+//         }
+//     },
+//     "parserOptions": {
+//         "ecmaVersion": "latest",
+//         "sourceType": "module"
+//     },
+//     "plugins": [
+//         "react",
+//         "@typescript-eslint"
+//     ],
+//     "rules": {
+//         "semi": ["warn", "always"],
+//         "quotes": ["warn", "double"]
+//     },
+// }
